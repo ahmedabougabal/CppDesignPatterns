c0b759c19d57f40dfea08e7d561f3a1e4991c07b
ahmedabougabal <ahmedabougabal@live.com>
2024-11-22 23:45:25 +0200

    Feat(design pattern): Implements abstract factory pattern to create a class factory to construct related-family objects with different specs - Adds base Factory interface with Door and Engine creation - Implements concrete factories for Gas and Electric car variants - Includes related component hierarchies (Door, Engine)

---
+++ abstractFactoryPattern/CarsManufacture.cpp
@@ -0,0 +1,118 @@
+#include <iostream>
+#include <memory>
+using namespace std;
+
+class Door
+{
+public:
+  Door() {};
+  virtual void open() = 0;
+};
+
+class GasCarDoor : public Door
+{
+public:
+  GasCarDoor()
+  {
+    cout << "this is the sound of a gas car door" << endl;
+  }
+  void open()
+  {
+    cout << "click" << "\n";
+  }
+};
+
+class ElectricCarDoor : public Door
+{
+  ElectricCarDoor()
+  {
+    cout << "this is the sound of an electric car door" << endl;
+  }
+  void open()
+  {
+    cout << "zzzzzzzshhhh" << endl;
+  }
+};
+
+class Engine
+{
+protected:
+  char sound[15];
+
+public:
+  Engine()
+  {
+    strcpy_s(this->sound, "");
+  }
+  virtual void Run() = 0;
+};
+
+class GasEngine : public Engine
+{
+public:
+  GasEngine()
+  {
+    strcpy_s(this->sound, "V12 vrrrrooomm");
+    cout << "making a gas engine sound" << endl;
+  }
+  void Run()
+  {
+    cout << this->sound << endl;
+    cout << "a gas engine roars as it accelerates" << "\n";
+  }
+};
+
+class ElectricEngine : public Engine
+{
+public:
+  ElectricEngine()
+  {
+    strcpy_s(this->sound, "SHHHHH");
+    cout << "making an electric engine" << endl;
+  }
+  void Run()
+  {
+    cout << this->sound << endl;
+    cout << "an electric engine meows as it cuddle" << "\n";
+  };
+};
+
+//* factories
+
+class Factory
+{
+public:
+  virtual unique_ptr<Door> createDoor() = 0;
+  virtual unique_ptr<Engine> createEngine() = 0;
+  virtual ~Factory() = default;
+};
+
+class GasCarFactory : public Factory
+{
+  //* Implement the pure virtual functions for every car factory
+public:
+  unique_ptr<Door> createDoor() override
+  {
+    return make_unique<GasCarDoor>();
+  }
+
+  unique_ptr<Engine> createEngine() override
+  {
+    return make_unique<GasEngine>();
+  }
+};
+
+class ElectricCarFactory : public Factory
+{
+  //* Implement the pure virtual functions for every car factory
+public:
+  unique_ptr<Door> createDoor() override
+  {
+    return make_unique<ElectricCarDoor>();
+  }
+
+  unique_ptr<Engine> createEngine() override
+  {
+    return make_unique<ElectricEngine>();
+  }
+};
