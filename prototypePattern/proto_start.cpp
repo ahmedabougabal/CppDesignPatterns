7cfdc21a79c3b50ba860fb58860ce1bbe05d7632
ahmedabougabal <ahmedabougabal@live.com>
2024-11-26 22:34:31 +0200

    feat: Implements foundation boilerplate for prototype pattern from scratch

---
+++ prototypePattern/proto_start.cpp
@@ -0,0 +1,66 @@
+#include <iostream>
+using namespace std;
+
+class Animal
+{
+protected:
+  char hair_color[10];
+  int hair_length, tail, weight, height, age;
+  int intelligence, stubborness, aggressiveness;
+
+public:
+  int set_hair_length(int len)
+  {
+    this->hair_length = len;
+  }
+  void set_hair_color(const char *color)
+  {
+    strcpy_s(this->hair_color, color);
+  }
+  void set_tail(int _tail)
+  {
+    this->tail = _tail;
+  }
+  void set_weight(int _weight)
+  {
+    this->weight = _weight;
+  }
+  void set_height(int _height)
+  {
+    this->height = _height;
+  }
+  void set_age(int _age)
+  {
+    this->age = _age;
+  }
+};
+
+// create the derived sheep class
+class Sheep : public Animal
+{
+public:
+  Sheep()
+  {
+    this->hair_length = 5;
+    this->stubborness = 3;
+    this->aggressiveness = 2;
+    this->intelligence = 7;
+  }
+};
+
+// create the derived cow class
+class Cow : public Animal
+{
+public:
+  Cow()
+  {
+    this->stubborness = 6;
+    this->aggressiveness = 5;
+    this->intelligence = 8;
+  }
+};
+
+int main()
+{
+  return 0;
+}
